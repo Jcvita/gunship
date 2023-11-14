ceac616cfbcd8ea16d2461ae7c79b2369d96c56a
Joseph Vita <jcvita2012@gmail.com>
2023-11-14 17:35:25 -0500

    desktop version

---
+++ src/bin/main.rs
@@ -0,0 +1,10 @@
+use ratatui::terminal::Terminal;
+use ratatui::widgets::{Block, Borders};
+use ratatui::layout::{Layout, Constraint, Direction};
+use ratatui::backend::CrosstermBackend;
+
+use std::io::stdout;
+
+fn main() {
+    
+}
---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use std::{io, sync::{Arc, Mutex}};
+use tui::*;
+
+use winit::event_loop::EventLoop;
+pub mod graphics;
+pub mod tui;
+
+pub struct User<'a> {
+    pub name: String,
+    pub files: Vec<&'a File<'a>>,
+}
+
+pub struct File<'a> {
+    pub path: String,
+    pub creator: &'a User<'a>,
+}
+
+pub struct AppState<'a> {
+    pub files: Vec<&'a File<'a>>,
+    pub users: Vec<&'a User<'a>>,
+}
+
+pub async fn run_graphics() -> () {
+    let event_loop = EventLoop::new().unwrap();
+    let window = winit::window::Window::new(&event_loop).unwrap();
+    
+    //graphics::run(event_loop, window).await
+}
+
+pub fn run_tui(state: Arc<Mutex<AppState>>) -> Result<(), io::Error> {
+    let mut terminal = setup_terminal()?;
+    run(&mut terminal, state)?;
+    restore_terminal(&mut terminal)?;
+    Ok(())
+}
